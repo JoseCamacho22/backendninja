258b95fe8020da1c6100dccbc1370a050880f130
Josecamacho22 <jcamachofernandez22@gmail.com>
2017-04-06 21:03:04 +0200

    Insertando datos Complejos

--- backendninja/src/main/java/com/udemy/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/controller/ExampleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.model.Person;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
@@ -18,7 +20,7 @@ public class ExampleController {
 	@GetMapping("/exampleString")
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	public String exampleString(Model model){
-		model.addAttribute("name", "Jose");
+		model.addAttribute("person", new Person("Jose", 29));
 		return EXAMPLE_VIEW;
 	}
 	
@@ -27,7 +29,7 @@ public class ExampleController {
 	//@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView exampleMAV(){
 		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("name", "Mario");
+		mav.addObject("person", new Person("Luis", 25));
 		return mav;
 		
 	}
