9d79ec6ea0bf21e5c44d16fb69c7619851d3ff5f
Josecamacho22 <jcamachofernandez22@gmail.com>
2017-04-08 13:35:22 +0200

    Añadimos componentes

--- backendninja/src/main/java/com/udemy/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/controller/ExampleController.java
@@ -4,12 +4,15 @@ package com.udemy.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.component.ExampleComponent;
 import com.udemy.model.Person;
 
 @Controller
@@ -19,10 +22,16 @@ public class ExampleController {
 	public static final String EXAMPLE_VIEW="example";//la plantilla
 
 	
+	@Autowired
+	@Qualifier("exampleComponent")
+	private ExampleComponent exampleComponent;
+	
+	
 	//Primer forma
 	@GetMapping("/exampleString")
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	public String exampleString(Model model){
+		exampleComponent.sayHello();
 		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
