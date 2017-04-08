24ab0a1ada4cf559e0bc14dbd3117b4c23dfb873
Josecamacho22 <jcamachofernandez22@gmail.com>
2017-04-08 13:58:56 +0200

    Controlando el tiempo de peticiones

---
+++ backendninja/src/main/java/com/udemy/component/RequestTimeInterceptor.java
@@ -0,0 +1,39 @@
+package com.udemy.component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+@Component("resquestTimeInterceptor")
+public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
+
+	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+	
+	//se ejecuta antes de entrar en el metodo del controlador
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		//antes de que sejecute guarda el tiempo actual
+		request.setAttribute("startTime", System.currentTimeMillis());
+		return true;
+	}
+
+	
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long startTime = (long) request.getAttribute("startTime");
+		LOG.info("--REQUEST URL: '" + request.getRequestURL().toString() + "' --TOTAL TIME : '" +(System.currentTimeMillis() -startTime)+ "ms");
+	}
+
+	
+
+	
+	
+}
