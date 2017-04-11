8787787c94c0e7eea10c4f651abbd48046af719c
Josecamacho22 <jcamachofernandez22@gmail.com>
2017-04-11 20:39:14 +0200

    Creamos nuestra vista y insertamos datos en la tabla

--- backendninja/src/main/java/com/udemy/repository/CourseJpaRepository.java
+++ backendninja/src/main/java/com/udemy/repository/CourseJpaRepository.java
@@ -11,12 +11,12 @@ import com.udemy.entity.Course;
 @Repository("courseJpaRepository")
 public interface CourseJpaRepository extends JpaRepository<Course, Serializable>{
 	
-	public abstract Course findByPrice(int price);
-	
-	public abstract Course findByPriceAndName(int price, String name);
-	
-	public abstract List<Course >findByNameOrderByHours(String name);
-	
-	public abstract Course findByNameOrPrice(String name,int price);
+//	public abstract Course findByPrice(int price);
+//	
+//	public abstract Course findByPriceAndName(int price, String name);
+//	
+//	public abstract List<Course >findByNameOrderByHours(String name);
+//	
+//	public abstract Course findByNameOrPrice(String name,int price);
 
 }
--- backendninja/src/main/java/com/udemy/service/impl/CourseServiceImpl.java
+++ backendninja/src/main/java/com/udemy/service/impl/CourseServiceImpl.java
@@ -2,15 +2,22 @@ package com.udemy.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 
 import com.udemy.entity.Course;
 import com.udemy.repository.CourseJpaRepository;
 import com.udemy.service.CourseService;
 
-public class CouserServiceImpl implements CourseService {
+
+@Service("courseServiceImpl")
+public class CourseServiceImpl implements CourseService {
 	
+	private static final Log LOG = LogFactory.getLog(CourseServiceImpl.class);
 	
 	@Autowired
 	@Qualifier("courseJpaRepository")
@@ -18,11 +25,13 @@ public class CouserServiceImpl implements CourseService {
 
 	@Override
 	public List<Course> listAllCourses() {
+		LOG.info("CALL : " + "listAllCourses()");
 		return courseJpaRepository.findAll();
 	}
 
 	@Override
 	public Course addCourse(Course course) {
+		LOG.info("CALL : " + "addCourse()");
 		return courseJpaRepository.save(course);
 	}
 
